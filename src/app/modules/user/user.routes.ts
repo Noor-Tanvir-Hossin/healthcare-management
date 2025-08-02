bea81de71f980539b315e800fc8ddafea0163968
Noor-Tanvir-Hossain <tanvirrobin11567@gmail.com>
2025-08-02 22:35:10 +0600

    implement auth middleware for role based access

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,8 +1,9 @@
 import express from 'express'
 import { userController } from './user.controller';
+import auth from '../../middlewares/auth';
 
 const router = express.Router();
 
-router.post('/create-admin', userController.createAdmin);
+router.post('/create-admin', auth("ADMIN") , userController.createAdmin);
 
 export const userRoutes = router;
