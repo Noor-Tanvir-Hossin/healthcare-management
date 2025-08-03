36a0265b454b266393490794b948873dc19d2f63
Noor-Tanvir-Hossain <tanvirrobin11567@gmail.com>
2025-08-03 19:47:07 +0600

    implement reusable ApiError class

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -2,17 +2,19 @@ import { NextFunction, Request, Response } from "express"
 import { jwtHelpers } from "../../helpars/jwtHelper"
 import config from "../../config"
 import { Secret } from "jsonwebtoken"
+import ApiError from "../errors/ApiError"
+import { StatusCodes } from "http-status-codes"
 
 const auth = (...roles: string[]) =>{
     return async (req:Request, res:Response, next:NextFunction) =>{
         try {
             const token = req.headers.authorization
         if(!token){
-            throw new Error("You are not authorize")
+            throw new ApiError(StatusCodes.UNAUTHORIZED,"You are not authorized")
         }
         const verifiedUser = jwtHelpers.verifyToken(token, config.jwt.jwt_secret as Secret)
         if(roles.length && !roles.includes(verifiedUser.role)){
-            throw new Error("You are not authorize")
+            throw new ApiError(StatusCodes.FORBIDDEN,"Forbidden !!")
         }
         next()
         } catch (err) {
