722e4c4d656d2fe00c9e725a1cc094007e139794
Noor-Tanvir-Hossain <tanvirrobin11567@gmail.com>
2025-09-18 23:18:02 +0600

    implement api for inserting schedule into db

--- src/app/modules/schedules/schedules.service.ts
+++ src/app/modules/schedules/schedules.service.ts
@@ -1,10 +1,71 @@
+import { addHours, addMinutes, format } from "date-fns";
+import prisma from "./../../../shared/prisma";
 
+const convertDateTime = async (date: Date) => {
+  const offset = date.getTimezoneOffset() * 60000;
+  return new Date(date.getTime() + offset);
+};
 
-const insertSchedulesIntoDB = async (payload:any) => {
-   
-  };
+const insertSchedulesIntoDB = async (payload: any) => {
+  const { startDate, endDate, startTime, endTime } = payload;
 
+  const intervalTime = 30;
+  const schedules = [];
 
-export const ScheduleService = {    
-    insertSchedulesIntoDB
-  };
+  const currentDate = new Date(startDate);
+  const lastDate = new Date(endDate);
+
+  while (currentDate <= lastDate) {
+    const startDateTime = new Date(
+      addMinutes(
+        addHours(
+          `${format(currentDate, "yyyy-MM-dd")}`,
+          Number(startTime.split(":")[0])
+        ),
+        Number(startTime.split(":")[1])
+      )
+    );
+
+    const endDateTime = new Date(
+      addMinutes(
+        addHours(
+          `${format(lastDate, "yyyy-MM-dd")}`,
+          Number(endTime.split(":")[0])
+        ),
+        Number(endTime.split(":")[1])
+      )
+    );
+
+    while (startTime < endTime) {
+      const s = await convertDateTime(startDateTime);
+      const e = await convertDateTime(addMinutes(startDateTime, intervalTime));
+
+      const scheduleData = {
+        startDateTime: s,
+        endDateTime: e,
+      };
+
+      const existingSchedule = await prisma.schedule.findFirst({
+        where: {
+          startDateTime: scheduleData.startDateTime,
+          endDateTime: scheduleData.endDateTime,
+        },
+      });
+
+      if (!existingSchedule) {
+        const result = await prisma.schedule.create({
+          data: scheduleData,
+        });
+        schedules.push(result);
+      }
+      startDateTime.setMinutes(startDateTime.getMinutes() + intervalTime);
+    }
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  return schedules;
+};
+
+export const ScheduleService = {
+  insertSchedulesIntoDB,
+};
