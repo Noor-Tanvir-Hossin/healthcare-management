2c38b754af8a98bcf8c69eefadf02dcf5271a52f
Noor-Tanvir-Hossain <tanvirrobin11567@gmail.com>
2026-04-24 20:42:42 +0600

    some changes in server.js

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,31 @@
 import { Server } from "http"
 import app from "./app"
+import config from "./config";
 
 
 const port = 3000
 
 async function main(){
     const server: Server = app.listen(port, ()=>{
-        console.log(`Server is running on ${port}`)
-     })
+        console.log(`Server is running on ${config.port}`)
+     });
+
+     const exitHandler = () => {
+        if (server) {
+            server.close(() => {
+                console.info("Server closed!")
+            })
+        }
+        process.exit(1);
+    };
+    process.on('uncaughtException', (error) => {
+        console.log(error);
+        exitHandler();
+    });
+
+    process.on('unhandledRejection', (error) => {
+        console.log(error);
+        exitHandler();
+    })
 }
 main()
